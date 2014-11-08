bbd7aad312bac7cc62a985be4e26a5a3a6c6687c
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-11-08 15:57:21 +0600

    Make a simple Calculator using JavaScript.

---
+++ JavaScriptsProject/SimpleCalculator/SimpleCalculator/public_html/Calculator.js
@@ -0,0 +1,33 @@
+//JavaScript Document
+function myFunction()
+{
+    var x = prompt("Enter 1 for +\n Enter 2 for - \n Enter 3 * \n Enter 4 for /");
+    if(x==1)
+    {
+        var firstNumber = Number(prompt("Enter your first number"));
+        var secondNumber = Number(prompt("Enter your first number"));
+        alert(add(firstNumber,secondNumber));
+
+        
+    }
+    else if(x==2)
+    {
+        
+    }
+    else if(x==3)
+    {
+        
+    }
+    else if(x==4)
+    {
+        
+    }
+}
+
+
+
+    
+function add(firstNo,secondNo)
+{
+    return firstNo + secondNo;
+}
