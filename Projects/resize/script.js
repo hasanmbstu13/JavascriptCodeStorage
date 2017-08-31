53998537f57dd317dad0ced4bf01aa4c7069b6f9
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-08-31 15:13:21 +0600

    Learn the differebt functionality of windows

--- Projects/resize/script.js
+++ Projects/resize/script.js
@@ -4,6 +4,15 @@
 // - height of horizontal scrollbar (if present).
 // The clientHeight property returns the viewable height of an element in pixels, 
 // including padding, but not the border, scrollbar or margin.
+// innerHeight - Get the current computed inner height (including padding but not border) for the first element 
+// in the set of matched elements or set the inner height of every matched element.
+// innerWidth - Get the current computed inner width (including padding but not border) for the first element 
+// in the set of matched elements or set the inner width of every matched element.
+// onload is most often used within the <body> element to execute a script once 
+// a web page has completely loaded all content (including images, script files, CSS files, etc.).
+// The onresize event occurs when the browser window has been resized.
+// Tip: To get the size of an element, 
+// use the clientWidth, clientHeight, innerWidth, innerHeight, outerWidth, outerHeight, offsetWidth and/or offsetHeight properties.
 function adjustStyle() {
 	var width = 0;
 	// get the width.. more cross-browser issues
