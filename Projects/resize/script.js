2bf928d730795266c4650930a878c1f1c1da54ba
hasanmbstu13 <hasanmbstu13@gmail.com>
2017-08-31 14:50:48 +0600

    Add the note of necessary elements

--- Projects/resize/script.js
+++ Projects/resize/script.js
@@ -1,3 +1,9 @@
+// document.body element returns the <body> element
+// document.documentElement returns the <html> element
+// clientHeight can be calculated as CSS height + CSS padding 
+// - height of horizontal scrollbar (if present).
+// The clientHeight property returns the viewable height of an element in pixels, 
+// including padding, but not the border, scrollbar or margin.
 function adjustStyle() {
 	var width = 0;
 	// get the width.. more cross-browser issues
